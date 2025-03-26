a7c628bfa4330f66318d7b48f1a8de83264f1806
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-03-26 15:55:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,11 +11,6 @@ const handler = require("./handlers");
 
 const bot = new TelegramBot(env.BOT_TOKEN, config.bot);
 
-		// Handlers
-		bot.on("callback_query", handler.callbackQuery(bot));
-		bot.on("polling_error", handler.botError);
-		bot.on("error", handler.botError);
-
 		console.info(`\u{1F41D} ${env.BOT_NAME} started successfully`);
 	})
 	.catch(error => console.error(error));
